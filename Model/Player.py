bb6341aef0626c0f81dc7c728017612bc350a2fe
Julien <julien.gergisarkis@gmail.com>
2021-10-03 22:10:06 +0300

    Added Central Bank and Player plus some attributes in the player

---
+++ Model/Player.py
@@ -0,0 +1,38 @@
+## Person
+gender
+age
+firstName
+lastName
+title
+
+## Getters and Setters
+def setFirstName(firstName){
+    this.firstName = firstName
+}
+def getName(){
+    return this.firstName
+}
+def setAge(age){
+    this.age = age
+}
+def getAge(){
+    return this.age
+}
+def setGender(gender){
+    this.gender = gender
+}
+def getGender(){
+    return this.gender
+}
+def setLastName(lastName){
+    this.lastName = lastName
+}
+def getLastName(){
+    return this.lastName
+}
+def setTitle(title){
+    this.title = title
+}
+def getTitle(){
+    return this.title
+}
